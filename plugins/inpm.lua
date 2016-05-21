52d2c97781033118ca6c536ea8602dc1f69084cf
pooryarajaee <pooryarajaee@gmail.com>
2016-05-21 10:46:21 +0430

    Update inpm.lua

--- plugins/inpm.lua
+++ plugins/inpm.lua
@@ -113,7 +113,7 @@ if to == 'user' or service or is_admin1(msg) and to == "chat" or to == "channel"
 		channel_invite(channel, user, ok_cb, false)
 	elseif matches[2]:lower() == 'persian' and matches[3]:lower() == 'support' then
 		savelog(msg.to.id, name_log.." ["..msg.from.id.."] tried to join Persian support")
-		local target = 1067186995
+		local target = 0
 		local long_id = data[tostring(target)]['long_id']
 		if is_banned(msg.from.id, tostring(target)) then
 			return 'You are banned.'
@@ -208,7 +208,7 @@ if to == 'user' or service or is_admin1(msg) and to == "chat" or to == "channel"
 
 	if matches[1] == 'help' and msg.to.type == 'user' or matches[1] == 'pmhelp' and is_admin1(msg) and msg.to.type ~= 'user' then
       	savelog(msg.to.id, name_log.." ["..msg.from.id.."] used pm help")
-		text = "Welcome to TelePlus!\n\nTo get a list of TelePlus groups use /chats or /chatlist for a document list of chats.\n\nTo get a new TelePlus group, contact a support group or bot:@PHAE2099\n\nOr\n\n@pooryapv_bot\n\nOr\n\nFor Persian support, use: /join Persian support\n\nFor more information, check out our channel:\n\n@TelePlus [Persian]\n\nThanks for using @P_TelePlus!"
+		text = "Welcome to TelePlus!\n\nTo get a list of TelePlus groups use /chats or /chatlist for a document list of chats.\n\nTo get a new TelePlus group, contact admin or bot:\n\n@PHAE2099[Admin]\n\nOr\n\n@pooryapv_bot\n\nFor more information, check out our channel:\n\n@TelePlus [Persian]\n\nThanks for using @P_TelePlus!"
      	return text
     end
 
