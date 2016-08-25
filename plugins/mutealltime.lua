98215d168dd45b89cbc1f07e552d1a01d82fc7ee
PRR78 <pooryarajaee78@gmail.com>
2016-08-26 01:29:13 +0430

    Update mutealltime.lua

--- plugins/mutealltime.lua
+++ plugins/mutealltime.lua
@@ -1,3 +1,5 @@
+-- by @Blackwolf_admin 
+-- channel : @open_sources
 do
 local function pre_process(msg)
  local hash = 'muteall:'..msg.to.id
@@ -12,26 +14,35 @@ local function run(msg, matches)
        local hash = 'muteall:'..msg.to.id
        if not matches[2] then
               redis:set(hash, true)
-             return "mute all has been enabled"
+             return "Mute all has been enabled"
  else
- local num = tonumber(matches[2]) * 60
- redis:setex(hash, num, true)
- return "mute all has been enabled for |#"..matches[2].."#| minutes"
+-- by @Blackwolf_admin 
+local hour = string.gsub(matches[2], 'h', '')
+ local num1 = tonumber(hour) * 3600
+local minutes = string.gsub(matches[3], 'm', '')
+ local num2 = tonumber(minutes) * 60
+local second = string.gsub(matches[4], 's', '')
+ local num3 = tonumber(second) 
+local num4 = tonumber(num1 + num2 + num3)
+redis:setex(hash, num4, true)
+ return "Mute all has been enabled for\n⏺ hour(s) : "..matches[2].."\n⏺ minute(s) : "..matches[3].." \n⏺ second(s) : "..matches[4]..""
  end
  end
 if matches[1] == 'unmuteall' and is_momod(msg) then
                local hash = 'muteall:'..msg.to.id
         redis:del(hash)
-          return "mute all has been disabled"
+          return "Mute all has been disabled"
   end
 end
 return {
    patterns = {
       '^[/!#](muteall)$',
       '^[/!#](unmuteall)$',
-   '^[/!#](muteall) (%d+)$',
+   '^[/!#](muteall) (.*) (.*) (.*)$',
  },
 run = run,
   pre_process = pre_process
 }
 end
+-- by @Blackwolf_admin 
+-- our channel : @open_sources 
